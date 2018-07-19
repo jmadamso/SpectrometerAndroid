c77d874a4ae984fdd93977d2a778fdf70d9e26bc
jmadamso <jmadamso@ucsc.edu>
2018-07-19 03:31:42 -0700

    Result screen overhaul

--- app/src/main/java/jmadamso/spectrometer/ResultActivity.java
+++ app/src/main/java/jmadamso/spectrometer/ResultActivity.java
@@ -2,104 +2,125 @@ package jmadamso.spectrometer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYSeries;
-import com.androidplot.xy.*;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.text.FieldPosition;
-import java.text.Format;
-import java.text.ParsePosition;
-import java.util.*;
 
 
-public class ResultActivity extends Activity {
 
-    private XYPlot plot;
+public class ResultActivity extends AppCompatActivity {
+
     static private double[] spectrumArray = new double[defines.NUM_WAVELENGTHS];
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int pos = tab.getPosition();
+                Toast.makeText(ResultActivity.this, "Tab at pos = " + pos , Toast.LENGTH_SHORT).show();
+                switch(pos) {
+                    case 0:
+                        //set view to the graph
+                        break;
 
-        // initialize our XYPlot reference:
-        plot = (XYPlot) findViewById(R.id.plot);
+                    case 1:
+                        //set view to info screen
+                        break;
+
+                    case 2:
+                        //set view to share screen
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+
+
+
 
 
         //grab the array that was passed in:
         Bundle b = this.getIntent().getExtras();
         spectrumArray = b.getDoubleArray("spectrumArray");
 
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        initGraph(graph);
 
-        // create a couple arrays of y-values to plot:
-        final Number[] domainLabels = new Number[1024];
-        Number[] series1Numbers = new Number[1024];
+
+
+
+
+
+
+    }
+
+    private void initGraph(GraphView graph) {
+
+
+        // first series is a line
+        DataPoint[] points = new DataPoint[defines.NUM_WAVELENGTHS];
+
+        double wavelengths[] = new double[1024];
 
         DecimalFormat df = new DecimalFormat("###.##");
         df.setRoundingMode(RoundingMode.HALF_UP);
-
         int i;
-        for(i = 0; i < series1Numbers.length; i++) {
-            series1Numbers[i] = spectrumArray[i];
-            domainLabels[i] = Double.parseDouble(df.format(defines.wavelengthArray[i]));
+
+        for (i = 0; i < points.length; i++) {
+            wavelengths[i] = Double.parseDouble(df.format(defines.wavelengthArray[i]));
+            points[i] = new DataPoint(wavelengths[i], spectrumArray[i]);
         }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
 
+        // set manual X bounds
+        //graph.getViewport().setYAxisBoundsManual(true);
+        //graph.getViewport().setMinY(-200);
+        //graph.getViewport().setMaxY(200);
 
+        //graph.getViewport().setXAxisBoundsManual(true);
+        //graph.getViewport().setMinX(4);
+        //graph.getViewport().setMaxX(80);
 
+        // enable scaling
+        graph.getViewport().setScalable(true);
 
+        series.setTitle("Spectrum");
 
-        // turn the above arrays into XYSeries':
-        // (Y_VALS_ONLY means use the element index as the x value)
-        XYSeries series1 = new SimpleXYSeries(
-                Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
+        graph.addSeries(series);
 
-        // create formatters to use for drawing a series using LineAndPointRenderer
-        // and configure them from xml:
-        LineAndPointFormatter series1Format =
-                new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels);
-
-
-
-        // just for fun, add some smoothing to the lines:
-        // see: http://androidplot.com/smooth-curves-and-androidplot/
-        //series1Format.setInterpolationParams(
-                //new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
-
-
-        // add a new series' to the xyplot:
-        plot.addSeries(series1, series1Format);
-        plot.getLegend().setVisible(false);
-        plot.setDomainStep(StepMode.SUBDIVIDE, 9);
-        plot.setRangeStep(StepMode.SUBDIVIDE, 6);
-
-        //plot.getGraph().position(
-                //100, HorizontalPositioning.ABSOLUTE_FROM_LEFT,
-                //100, VerticalPositioning.ABSOLUTE_FROM_TOP);
-        //plot.getGraph().setPadding(0,0,0,0);
-        //plot.setMarkupEnabled(true);
-
-        //plot.getGraph().setSize(new Size(
-                //PixelUtils.dpToPix(50), SizeMode.FILL,
-                //PixelUtils.dpToPix(50), SizeMode.FILL));
-
-        //MARGIN is where labels go. increase this is range labels are cutoff
-        plot.getGraph().setMarginLeft(200);
-
-
-        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
-            @Override
-            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
-                int i = Math.round(((Number) obj).floatValue());
-                return toAppendTo.append(domainLabels[i]);
-            }
-
-            @Override
-            public Object parseObject(String source, ParsePosition pos) {
-                return null;
-            }
-        });
-
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
     }
 }
